bf04911e50ac85c744050c2af4b02eecd46dbc73
Samyakkumar <samyakkumar@gmail.com>
2019-06-03 12:29:19 +0200

    Add Home Component

---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+    } 
+
+    render() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        )
+    }
+
+}
+
+
+export default HomePage;
