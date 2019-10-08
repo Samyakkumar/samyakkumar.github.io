088828c52a444d46acc54375c608070c136fab1a
Samyakkumar <samyakkumar@gmail.com>
2019-10-08 04:05:28 -0400

    More stuff

--- client/src/components/HomePage/Window.js
+++ client/src/components/HomePage/Window.js
@@ -11,10 +11,17 @@ class Window{
         this.p.fill(0, 0, 0);
         this.p.rect(this.x - this.length, this.y - this.width, this.x + this.length, this.y + this.width);
         this.p.fill(255, 255, 255)
+        //right most
         this.p.rect(this.x - this.length + 5.4*this.length , this.y - this.length, this.x + this.length - 5.4*this.length, this.y + this.length - 4.8*this.length)
+        // middle
         this.p.rect(this.x - this.length + 5.1*this.length , this.y - this.length, this.x + this.length - 5.4*this.length, this.y + this.length - 4.8*this.length)
+        //left most
         this.p.rect(this.x - this.length + 4.8*this.length , this.y - this.length, this.x + this.length - 5.4*this.length, this.y + this.length - 4.8*this.length)
 
+
+    }
+
+    mouseIn() {
 
     }
 }
