c87c07d9780aef62521ccaa1c53f25cc11e391af
Samyakkumar <samyakkumar@gmail.com>
2020-06-04 20:13:55 +0530

    Fix herokupostinstall; add google analytics

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import ReactGA from 'react-ga';
 import Resume from "../Resume/Resume";
 import Sketch from "../Sketch/Sketch";
 export default function Home() {
+    ReactGA.initialize("UA-141601792-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
     const [count, setCount] = useState(0);
     return(
         <div>
