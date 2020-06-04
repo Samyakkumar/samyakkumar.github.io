6d0d59103e9633d1415f65775e3af66d325d45ed
Samyakkumar <samyakkumar@gmail.com>
2020-06-04 20:00:55 +0530

    New build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8ba04ad93c199e390011ac62c198f77.js"
+  "/precache-manifest.1c96713ae5f6ba5583d407f262c66aa8.js"
 );
 
 self.addEventListener('message', (event) => {
